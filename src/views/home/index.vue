3e12c38d840ef8f58b1c8c28f67ccd12db9f9fb8
kscript <1258@live.cn>
2019-02-21 18:08:46 +0800

    自动选中当前路由所在的菜单项

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,23 +1,27 @@
 <template>
   <div class="view-home">
     <el-container>
-    <el-aside class="scroll">
+    <el-aside class="scroll" :width="asideW + 'px'">
       <!-- <el-radio-group v-model="isCollapse" style="margin-bottom: 20px;">
         <el-radio-button :label="false">展开</el-radio-button>
         <el-radio-button :label="true">收起</el-radio-button>
       </el-radio-group> -->
       <el-menu
-       default-active="0-0-0"
        class="el-menu-vertical-demo"
-       @open="handleOpen"
-       @close="handleClose"
+       text-color="#fff"
+       background-color="#545c64"
+       active-text-color="#ffd04b"
+       @select="selectMenu"
        :collapse="isCollapse"
        :collapse-transition="false"
-       background-color="#545c64"
-       text-color="#fff"
-       active-text-color="#ffd04b">
+       :default-active="defaultActive"
+       :style="'width: ' + asideW + 'px'"
+       >
         <template v-for="(vo, i1) in list">
-        <el-submenu :index="String(i1)" v-if="vo.children" :key="i1">
+        <el-submenu
+         :index="String(i1)"
+         v-if="vo.children"
+         :key="i1">
           <template slot="title">
             <i :class="vo.icon" v-if="vo.icon"></i>
             <span>
@@ -36,6 +40,7 @@
             <el-menu-item 
              v-for="(v3, i3) in v2.group"
              :key="i3"
+             :route="v3.to.path"
              :index="i1 + '-' + i2 + '-' + i3"
              >
              {{v3.label}}
@@ -60,26 +65,34 @@
 export default {
   data () {
     return {
+      asideW: 240,
       isCollapse: false,
+      defaultActive: '0-0-0',
       list: [
         {
-          to: {
-            path: '____'
-          },
+          label: '开始',
+          icon: 'el-icon-star-off',
+          children: [
+            {
+              label: '',
+              group: [
+                {
+                  to: {
+                    path: '/'
+                  },
+                  label: '使用说明'
+                }
+              ]
+            }
+          ]
+        },
+        {
           label: 'CSS',
           icon: 'el-icon-picture-outline',
-          
           children: [
             {
               label: '工具',
               group: [
-                {
-                  to: {
-                    path: '/sprite'
-                  },
-                  icon: 'el-icon-star-off',
-                  label: '精灵图片生成器'
-                },
                 {
                   to: {
                     path: '/sprite'
@@ -102,12 +115,30 @@ export default {
     }
   },
   methods: {
-    handleOpen (key, keyPath) {
-      console.log(key, keyPath)
+    selectMenu (index, indexPath, { route }) {
+      if (route !== '') {
+        this.$router.push({
+          path: route
+        })
+      }
     },
-    handleClose (key, keyPath) {
-      console.log(key, keyPath)
+    getActive () {
+      let path = this.$route.path
+      let active
+      this.list.forEach((vo, i1) => {
+        !active && (vo.children || []).forEach((v2, i2) => {
+          !active && (v2.group || []).forEach((v3, i3) => {
+            if (!active && v3.to.path === path) {
+              active = [i1, i2, i3].join('-')
+            }
+          })
+        })
+      })
+      return active
     }
+  },
+  mounted () {
+    this.defaultActive = this.getActive()
   }
 }
 </script>
@@ -120,16 +151,23 @@ export default {
   left: 0;
 }
 .el-aside{
+  // position: fixed;
+  // top: 0;
+  // bottom: 0;
+  // left: 0;
+  // width: auto!important;
+  // min-width: 200px;
+  background: #545c64;
+}
+.el-menu{
   position: fixed;
   top: 0;
   bottom: 0;
   left: 0;
-  width: auto!important;
-  min-width: 200px;
-  background: #545c64;
-}
-.el-menu{
   border-right: none;
+  /deep/ .el-menu-item-group__title{
+    display: none;
+  }
 }
 </style>
 
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 
 module.exports = {
+  publicPath: './',
+  outputDir: 'docs',
   productionSourceMap: false,
   lintOnSave: false,
   devServer: {

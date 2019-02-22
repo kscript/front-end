3025d604e945b37240e7892d7d21baa3212d044f
kscript <1258@live.cn>
2019-02-22 09:32:06 +0800

    添加axios

--- src/main.js
+++ src/main.js
@@ -2,12 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from './axios'
 import elementUI from 'element-ui'
 import {eventBus} from './util/eventBus'
 import 'element-ui/lib/theme-chalk/index.css';
 Vue.use(elementUI)
 
 Vue.use(eventBus)
+Vue.use(axios)
 
 Vue.config.productionTip = false
 
